169e46cb952950dcaf2a1599cd18007e2c61e4d2
kongxiaoxiang <251314155@qq.com>
2020-05-16 12:05:34 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,54 +1,29 @@
-//引入react核心库
-  import React,{Component} from 'react'
-//引入自己定义的组件
-  import Add from './components/Add'
-  import List from './components/List'
-//定义组件，并暴露
-  export default class App extends Component{
-    state = {
-      comments:[
-        {id:001,username:'xiaokong',content:'react is bangbangda~~~~'},
-        {id:002,username:'xiaozhang',content:'react is good~~~~'},
-        {id:003,username:'xiaozhao',content:'react is difficult~~~~'}
-      ]
-    }
-    //添加用户评论内容
-    addComments = (commentObj)=>{
-      //获取原状态
-      const {comments} = this.state
-      //更新状态
-      this.setState({comments:[commentObj,...comments]})
-    }
-    //删除用户评论
-    deleteComments = (id)=>{
-      //获取原状态
-      const {comments} = this.state
-      comments.findIndex((commentObj) =>{
-        return commentObj.id === id
-      })
-      comments.splice(index,1)
-      //更新状态
-      this.setState({comments})
-    } 
-    render(){
-      return (
-        <div id="app">
-          <div>
-            <header className="site-header jumbotron">
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-xs-12">
-                        <h1>请发表对React的评论</h1>
-                      </div>
-                    </div>
-                  </div>
-                </header>
-              <div className="container">
-                <Add addComments={this.addComments}/>
-                <List comments={this.state.comments} deleteComments={this.deleteComments}/>
-              </div>
-          </div>
-        </div>
-      )
+import React,{Component} from 'react'
+import axios from 'axios'
+export default class App extends Component{
+  state = {
+    respoName:'',
+    respoUrl:'',
+    isLoading:true,
+    err:'',//错误信息
+    keyWord:'r'
+  }
+  async componentDidMount(){
+    const url = `https://api.github.com/search/repositories?q=${this.state.keyWord}&sort=stars`
+    try {
+      let result = await axios.get(url)
+      const {name,html_url} = result.data.items[0]
+      this.setState({respoName:name,respoUrl:html_url,isLoading:false})
+    } catch (error) {
+      this.setState({isLoading:false,err:error.message})
     }
   }
+  render(){
+    const {keyWord,respoUrl,respoName,err,isLoading} = this.state
+    let content = ''
+    if(isLoading) content = <h2>Loading...</h2>
+    else if(err) content = <h2 style={{color:'red'}}>{err}</h2>
+    else content = <h2>在github上面以【{keyWord}】字母开头的仓库，点赞量最多的是:<a href= {respoUrl}>{respoName}</a></h2>
+    return content
+  }
+}
